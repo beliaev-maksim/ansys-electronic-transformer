d08b2c9eb3d443a0a3f440ea45731fa86dd49871
Maksim Beliaev <maksim.beliaev@ansys.com>
2021-02-02 00:14:34 +0100

    added geometry tests for IEEE

--- unittests/test_examples.py
+++ unittests/test_examples.py
@@ -2,6 +2,7 @@ import json
 import os
 from unittest import TestCase
 from AEDTLib.Desktop import Desktop
+from AEDTLib.Maxwell import Maxwell3D
 
 import src.ElectronicTransformer.etk_callback as etk
 
@@ -11,6 +12,7 @@ class BaseAEDT(TestCase):
     project = None
     tests_dir = None
     root_dir = None
+    modeler = None
 
     @classmethod
     def setUpClass(cls):
@@ -23,6 +25,13 @@ class BaseAEDT(TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.desktop.release_desktop(close_projects=True)
+
+    def vertex_from_edge_coord(self, coord, name, sort_key=1):
+        edge = self.modeler.primitives.get_edgeid_from_position(coord, obj_name=name)
+        vertices = self.modeler.primitives.get_edge_vertices(edge)
+        vertex_coord = [self.modeler.primitives.get_vertex_position(vertex) for vertex in vertices]
+        vertex_coord.sort(key=lambda x: x[sort_key])
+        return vertex_coord
 
 
 class TestIEEE(BaseAEDT):
@@ -38,6 +47,54 @@ class TestIEEE(BaseAEDT):
 
         cls.transformer.project = cls.project
         cls.transformer.setup_analysis()
+        cls.modeler = Maxwell3D().modeler
 
-    def test_ieee(self):
-        pass
+    def test_airgap(self):
+        """
+        Check that airgap exists only on central leg
+        """
+        vertex_coord = self.vertex_from_edge_coord((0, 0, 1.08), "I_Core")
+        self.assertListEqual(vertex_coord[0], [0.0, -2.5, 1.08])
+        self.assertListEqual(vertex_coord[1], [0.0, 2.5, 1.08])
+
+        vertex_coord = self.vertex_from_edge_coord((0, 0, 0.98), "E_Core")
+        self.assertListEqual(vertex_coord[0], [0.0, -2.5, 0.98])
+        self.assertListEqual(vertex_coord[1], [0.0, 2.5, 0.98])
+
+        vertex_coord = self.vertex_from_edge_coord((-5.5, 0, 1.08), "E_Core")
+        self.assertListEqual(vertex_coord[0], [-5.5, -2.5, 1.08])
+        self.assertListEqual(vertex_coord[1], [-5.5, 2.5, 1.08])
+
+    def test_layer_turns(self):
+        """
+        Check that only 7 turns are created on layer 8
+        """
+        layers_list = self.modeler.get_matched_object_name("Layer8*")
+        self.assertEqual(len(layers_list), 7)
+
+    def test_conductor_dimensions(self):
+        # width
+        vertex_coord = self.vertex_from_edge_coord((0, 5.005, 0.27), "Layer3_3")
+        self.assertListEqual(vertex_coord[0], [0.0, 4.32, 0.27])
+        self.assertListEqual(vertex_coord[1], [0.0, 5.69, 0.27])
+
+        # height
+        vertex_coord = self.vertex_from_edge_coord((0, 5.69, 0.24), "Layer3_3", sort_key=2)
+        self.assertListEqual(vertex_coord[0], [0.0, 5.69, 0.2])
+        self.assertListEqual(vertex_coord[1], [0.0, 5.69, 0.27])
+
+    def test_board_dimensions(self):
+        # legth X
+        vertex_coord = self.vertex_from_edge_coord((-2, 6.5, 1.01), "Board_8", sort_key=0)
+        self.assertListEqual(vertex_coord[0], [-5.5, 6.5, 1.01])
+        self.assertListEqual(vertex_coord[1], [0.0, 6.5, 1.01])
+
+        # height Z
+        vertex_coord = self.vertex_from_edge_coord((0, 6.5, 0.9), "Board_8", sort_key=2)
+        self.assertListEqual(vertex_coord[0], [0.0, 6.5, 0.81])
+        self.assertListEqual(vertex_coord[1], [0.0, 6.5, 1.01])
+
+        # width Y
+        vertex_coord = self.vertex_from_edge_coord((0, 4, 1.01), "Board_8", sort_key=2)
+        self.assertListEqual(vertex_coord[0], [0.0, 2.5, 1.01])
+        self.assertListEqual(vertex_coord[1], [0.0, 6.5, 1.01])
